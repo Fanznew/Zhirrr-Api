f8c8839e51a216811d49b2bc4bf28a0532ce1f61
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-16 01:05:54 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -12,10 +12,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'Fanz-AI',
+            namaowner: 'FanzOffc',
+            instagram: '-',
+            youtube : '-'
         }
     }
     res.json(config)
