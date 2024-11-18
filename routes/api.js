05b02bdc6be4ca76b0e846b4e42c7b654ca3d05b
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-18 10:14:43 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -358,7 +358,7 @@ router.get('/removebg', async (req, res) => {
     
     if (!apikeyInput) return res.json(loghandler.notparam);
     if (apikeyInput !== 'FanzOffc') return res.json(loghandler.invalidKey);
-    if (!username) return res.json(loghandler.notusername);
+    if (!url) return res.json(loghandler.noturl);
     if (!image) return res.json({ status: false, message: 'Parameter "image" tidak ditemukan' });
     
     try {
